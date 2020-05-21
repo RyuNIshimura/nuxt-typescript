0641dede9a564aadc987765f30590e6a61d3d1a1
RyuNIshimura <ryryrnsmr201@gmail.com>
2020-05-21 22:28:03 +0900

    add: Prettier

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,5 +1,20 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: [
-    '@nuxtjs/eslint-config-typescript'
-  ]
-}
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/vue",
+  ],
+  plugins: ["vue"],
+  rules: {
+    "vue/html-closing-bracket-newline": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "space-before-function-paren": 0,
+  },
+};
